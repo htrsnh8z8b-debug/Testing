e1fabb1612a0fcb3cf9372379f6d8d5021deadd8
htrsnh8z8b-debug <htrsnh8z8b@privaterelay.appleid.com>
2026-07-26 03:12:59 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,7 +3,7 @@ while not game:IsLoaded() do task.wait(0.1) end
 local ac = (gethui and gethui()) or game:GetService("CoreGui")
 
 local bc = loadstring(game:HttpGet('https://raw.githubusercontent.com/htrsnh8z8b-debug/Testing/refs/heads/main/Source.lua'))()
-local ci = loadstring(game:HttpGet('https://github.com/htrsnh8z8b-debug/Testing/blob/main/ThemeManager.lua'))()
+local ci = loadstring(game:HttpGet('https://raw.githubusercontent.com/htrsnh8z8b-debug/Testing/refs/heads/main/ThemeManager.lua'))()
 local bx = loadstring(game:HttpGet('https://raw.githubusercontent.com/htrsnh8z8b-debug/Testing/refs/heads/main/Settings.lua'))()
 
 local dc = bc:CreateWindow({
